82b78be0b51c5e8d4f58451d414efd43934c6a49
Jicksonsmokie <jickson.s@ENTERPRISE.COM>
2023-05-17 17:42:28 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (<div><input type='text' id='name' placeholder='name'/></div>
+  const [value, setValue] = useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const showAlert = () => {
+    alert(value);
+  };
+  return (
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+      <button onClick={showAlert}>Show Alert</button>
+    </div>
   );
 }
 
